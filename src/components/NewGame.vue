7593c5ab0760acce867cb08204088659ce4f837a
Nicola Pompini <nicola.pompini@gmail.com>
2022-07-16 01:56:56 +0200

    Added new game style

--- src/components/NewGame.vue
+++ src/components/NewGame.vue
@@ -1,5 +1,29 @@
 <template>
-   <p>TODO new game</p> 
+    <div class="container-flex mt-5">
+    <form class="row justify-content-center">
+        <button type="submit" class="btn btn-primary mx-4 col-3 my-auto">Start new game</button>
+        <div class="form-check text-start col-2 my-auto mx-0">
+            <input class="form-check-input my-1" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+                Player vs Player
+            </label>
+        </div>
+        <div class="col-6 row">
+            <div class="form-check text-start col-5 my-auto">
+                <input class="form-check-input my-1" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Player vs Computer
+                </label>
+            </div>
+            <div class="form-check text-start col-4 my-auto">
+                <input class="form-check-input my-1" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Play first turn
+                </label>
+            </div>
+        </div>
+    </form>
+    </div>
 </template>
 
 <script>
@@ -7,3 +31,9 @@ export default {
     name: "NewGame",
 }
 </script>
+
+<style scoped>
+.form-check {
+    font-size: .85em;
+}
+</style>
