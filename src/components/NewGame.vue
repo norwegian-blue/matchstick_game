d4aeec20b65ccda03aff6240c852c5fbc5151dc5
Nicola Pompini <nicola.pompini@gmail.com>
2022-07-16 22:58:26 +0200

    Fixed new game component display and added bindings to form fields

--- src/components/NewGame.vue
+++ src/components/NewGame.vue
@@ -1,23 +1,36 @@
 <template>
     <div class="container-flex mt-5">
-    <form class="row justify-content-center">
-        <button type="submit" class="btn btn-primary mx-4 col-3 my-auto">Start new game</button>
-        <div class="form-check text-start col-2 my-auto mx-0">
-            <input class="form-check-input my-1" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
+    <form class="row justify-content-center align-items-center" @submit.prevent="startNewGame">
+        <!-- Start new game -->
+        <button type="submit" class="btn btn-primary col-auto mx-4">Start new game</button>
+
+        <!-- Player 1 vs Player 2-->
+        <div class="form-check text-start col-auto" style="width:fit-content">
+            <input class="form-check-input my-1" type="radio" name="radioPlayers" id="radioPvP" value="PvP" v-model="matchModeRadio">
+            <label class="form-check-label" for="radioPvP">
                 Player vs Player
             </label>
         </div>
-        <div class="col-6 row">
-            <div class="form-check text-start col-5 my-auto">
-                <input class="form-check-input my-1" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                <label class="form-check-label" for="flexRadioDefault2">
+
+        <!-- PC versus PC -->
+        <div class="form-check text-start col-auto">
+            <input class="form-check-input my-1" type="radio" name="radioPlayers" id="radioCvC" value="CvC" v-model="matchModeRadio">
+            <label class="form-check-label" for="radioCvC">
+                Computer vs Computer
+            </label>
+        </div>
+
+        <!-- Player vs PC -->
+        <div class="col-auto row">
+            <div class="form-check text-start col-auto">
+                <input class="form-check-input my-1" type="radio" name="radioPlayers" id="radioPvC" value="PvC" v-model="matchModeRadio">
+                <label class="form-check-label" for="radioPvC">
                     Player vs Computer
                 </label>
             </div>
-            <div class="form-check text-start col-4 my-auto">
-                <input class="form-check-input my-1" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
+            <div class="form-check text-start col-auto">
+                <input class="form-check-input my-1" type="checkbox" value="" id="checkPfirst" v-model="playerFirstCheck" :disabled="matchModeRadio !== 'PvC'">
+                <label class="form-check-label" for="checkPfirst">
                     Play first turn
                 </label>
             </div>
@@ -29,6 +42,18 @@
 <script>
 export default {
     name: "NewGame",
+    data() {
+        return {
+            playerFirstCheck: false,
+            matchModeRadio: "PvP",
+        }
+    },
+    methods: {
+        startNewGame() {
+            console.log(`Player first? ${this.playerFirstCheck}`);
+            console.log(`Match mode? ${this.matchModeRadio}`)
+        }
+    }
 }
 </script>
 
